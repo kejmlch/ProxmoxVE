0f9c1849795630b0fe25578766e36e631daf4531
kejmlch <56795014+kejmlch@users.noreply.github.com>
2025-01-11 15:01:17 +0100

    Update alpine-docker.sh

--- custom-scripts-kejml/ct/alpine-docker.sh
+++ custom-scripts-kejml/ct/alpine-docker.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Spuštění původního skriptu z community-scripts
+# Spuštění původního skriptu a uložení výstupu
 echo "Spouštím původní alpine-docker.sh z community-scripts..."
-source <(wget -qO - https://github.com/community-scripts/ProxmoxVE/raw/main/ct/alpine-docker.sh)
+output=$(bash <(wget -qO - https://github.com/community-scripts/ProxmoxVE/raw/main/ct/alpine-docker.sh))
+
+# Extrakce VMID a MAC_ADDRESS z výstupu
+VMID=$(echo "$output" | grep -oP 'Using Virtual Machine ID: \K\d+')
+MAC_ADDRESS=$(echo "$output" | grep -oP 'Using MAC Address: \K[0-9A-Fa-f:]+')
+
+# Zobrazení výsledků
 echo "VMID je $VMID a MAC adresa je $MAC_ADDRESS"
