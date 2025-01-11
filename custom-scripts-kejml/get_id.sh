d94920adb8c1fbcd30ee9129749c9abd6a551c92
kejmlch <56795014+kejmlch@users.noreply.github.com>
2025-01-11 15:44:26 +0100

    Update get_id.sh

--- custom-scripts-kejml/get_id.sh
+++ custom-scripts-kejml/get_id.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
 
-# Zjištění ID naposledy vytvořeného VM
-LATEST_VM_CONFIG=$(ls -t /etc/pve/qemu-server/ | head -n 1)
-LATEST_VM_ID=${LATEST_VM_CONFIG%%.conf}
+# Získání poslední vytvořené operace z logu
+LOG_ENTRY=$(grep 'create' /var/log/pve/tasks/index | tail -n 1)
 
-# Zjištění ID naposledy vytvořeného CT
-LATEST_CT_CONFIG=$(ls -t /etc/pve/lxc/ | head -n 1)
-LATEST_CT_ID=${LATEST_CT_CONFIG%%.conf}
-
-# Porovnání a určení naposledy vytvořeného objektu
-if [ -z "$LATEST_VM_ID" ]; then
-  VMID=$LATEST_CT_ID
+# Extrakce ID a typu
+VMID=$(echo "$LOG_ENTRY" | grep -oP '\svm/\K\d+|\slxc/\K\d+')
+if echo "$LOG_ENTRY" | grep -q 'lxc/'; then
   TYPE="CT"
-elif [ -z "$LATEST_CT_ID" ]; then
-  VMID=$LATEST_VM_ID
+elif echo "$LOG_ENTRY" | grep -q 'vm/'; then
   TYPE="VM"
 else
-  if [ "$LATEST_VM_ID" -gt "$LATEST_CT_ID" ]; then
-    VMID=$LATEST_VM_ID
-    TYPE="VM"
-  else
-    VMID=$LATEST_CT_ID
-    TYPE="CT"
-  fi
+  echo "Nepodařilo se určit typ posledního vytvořeného objektu."
+  exit 1
 fi
-
-echo "Naposledy vytvořený objekt je typu: $TYPE s ID: $VMID"
 
 # Získání MAC adresy
 if [ "$TYPE" == "VM" ]; then
-  MAC_ADDRESS=$(qm config $VMID | grep -oP 'net\d+: \K.*' | grep -oP 'macaddr=\K[^,]+')
-else
-  MAC_ADDRESS=$(pct config $VMID | grep -oP 'net\d+: \K.*' | grep -oP 'hwaddr=\K[^,]+')
+  MAC_ADDRESS=$(qm config "$VMID" | grep -oP 'macaddr=\K[^,]+')
+elif [ "$TYPE" == "CT" ]; then
+  MAC_ADDRESS=$(pct config "$VMID" | grep -oP 'hwaddr=\K[^,]+')
 fi
 
+# Výstup výsledků
+echo "Naposledy vytvořený objekt je typu: $TYPE s ID: $VMID"
 echo "MAC adresa: $MAC_ADDRESS"
